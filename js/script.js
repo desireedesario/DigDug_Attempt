43e028e8637819a8fedeec7ff883df542fd71bef
Desiree Desario <desireedesario@gmail.com>
2016-07-08 03:31:14 -0700

    audio embedded

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ var downKey;
 var leftKey;
 var rightKey;
 
-var clear = [];
+var clear = 0;
 
 //speed of main character
 var speed = 4;
@@ -54,12 +54,12 @@ var pookah3;
 var fygar1;
 var fygar2;
 var win;
+var sound;
 
-var trackFacing = 0;
-var upPump;
-var downPump;
-var leftPump;
-var rightPump;
+var trackFacing = 1;
+
+var pump;
+var pumpExists = false;
 
 
 
@@ -82,7 +82,7 @@ function create() {
     pookah = game.add.group();
     fygar = game.add.group();
     winner = game.add.group();
-    pump = game.add.group();
+
 
     //CONTROLED DIGGER
     digger = game.add.sprite(100, 100, 'digger');
@@ -103,19 +103,19 @@ function create() {
     rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
     pumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-    pookah1 = pookah.create(150, 250, 'pookah');
+    pookah1 = pookah.create(150, 750, 'pookah');
     pookah1.anchor.setTo(0.5, 0.5);
     pookah1.animations.add('walk', [0, 1], 2, true)
     pookah1.animations.play('walk', [0, 1], 2, true)
     game.add.tween(pookah1).to({
         y: pookah1.y + 100
     }, 1500, 'Linear', true, 0, 100, true);
-    pookah2 = pookah.create(550, 370, 'pookah');
+    pookah2 = pookah.create(550, 270, 'pookah');
     pookah2.anchor.setTo(0.5, 0.5);
     pookah2.animations.add('walk', [0, 1], 2, true)
     pookah2.animations.play('walk', [0, 1], 2, true)
     game.add.tween(pookah2).to({
-        y: pookah2.y + 100
+        x: pookah2.x + 100
     }, 1500, 'Linear', true, 0, 100, true);
     pookah3 = pookah.create(500, 500, 'pookah');
     pookah3.anchor.setTo(0.5, 0.5);
@@ -131,13 +131,13 @@ function create() {
     game.add.tween(fygar1).to({
         x: fygar1.x + 100
     }, 1500, 'Linear', true, 0, 100, true);
-    fygar2 = fygar.create(550, 150, 'fygar');
+    fygar2 = fygar.create(550, 750, 'fygar');
     fygar1.anchor.setTo(0.5, 0.5);
     fygar1.animations.add('walk', [0, 1], 2, true)
     fygar1.animations.play('walk', [0, 1], 2, true)
     game.add.tween(fygar2).to({
         x: fygar2.x + 100
-    }, 500, 'Linear', true, 0, 100, true);
+    }, 1500, 'Linear', true, 0, 100, true);
 
 }
 
@@ -148,6 +148,8 @@ function create() {
 //function called once every frame, ideally 60 times per second
 function update() {
 
+
+
     //removing layers on map function
     map.setTileIndexCallback(0, this.digSoil, this);
 
@@ -155,176 +157,185 @@ function update() {
         trackFacing = 0;
         digger.animations.play('walk', 5, false);
         digSoil();
-        if (digger.y > 50)
-            digger.y -= speed;
-        if (digger.scale.x == -1)
-            digger.angle = 90;
-        else
-            digger.angle = -90;
+        digger.y -= speed;
+        if (trackFacing == 0) {
+            digger.angle = -90
+        }
 
     } else if (downKey.isDown) {
-        trackFacing = 1;
-        digger.animations.play('walk', 5, false);
-        digSoil();
-        if (digger.y < 900)
-            digger.y += speed;
-        if (digger.scale.x == -1)
-            digger.angle = -90;
-        else
-            digger.angle = 90;
-
-    } else if (leftKey.isDown) {
         trackFacing = 2;
         digger.animations.play('walk', 5, false);
         digSoil();
-        if (digger.x > 50)
-            digger.x -= speed;
-        digger.scale.x = -1;
-        digger.angle = 0;
-
-    } else if (rightKey.isDown) {
+        digger.y += speed;
+        if (trackFacing == 2) {
+            digger.angle = 90
+        }
+    } else if (leftKey.isDown) {
         trackFacing = 3;
         digger.animations.play('walk', 5, false);
         digSoil();
-        if (digger.x < 750)
-            digger.x += speed;
-        digger.scale.x = 1;
-        digger.angle = 0;
+        digger.x -= speed;
+        if (trackFacing == 3) {
+            digger.scale.y = -1;
+            digger.angle = 180;
+        }
+    } else if (rightKey.isDown) {
+        trackFacing = 1;
+        digger.animations.play('walk', 5, false);
+        digSoil();
+        digger.x += speed;
+        if (trackFacing == 1) {
+            digger.scale.y = 1;
+            digger.angle = 0;
+        }
     } else {
         digger.animations.stop('stand', 3, true);
     };
 
     if (trackFacing == 0) {
         if (pumpButton.isDown) {
-            upPump = pump.create(digger.x, digger.y, 'pump');
-            upPump.angle = -90;
-            upPump.enableBody = true;
-            game.physics.arcade.enable(upPump);
-            upPump.body.velocity.y = -1000;
-            upPump.scale.x = 1;
-            game.physics.arcade.overlap(upPump, pookah1, killPookah1);
-            game.physics.arcade.overlap(upPump, pookah2, killPookah2);
-            game.physics.arcade.overlap(upPump, pookah3, killPookah3);
-            game.physics.arcade.overlap(upPump, fygar1, killFygar1);
-            game.physics.arcade.overlap(upPump, fygar2, killFygar2);
-        } else {
-
+            if (!pumpExists) {
+                	$("#pump").get(0).play(); //gets the first element of the sound
+                pump = game.add.sprite(digger.x, digger.y, 'pump');
+                pump.angle = -90;
+                pump.enableBody = true;
+                game.physics.arcade.enable(pump);
+                pump.physicsBodyType = Phaser.Physics.ARCADE;
+                pump.body.velocity.y = -1000;
+                pumpExists = true;
+                game.time.events.add(Phaser.Timer.SECOND * 1, reload);
+            }
         }
     };
 
     if (trackFacing == 1) {
         if (pumpButton.isDown) {
-            downPump = pump.create(digger.x, digger.y, 'pump');
-            downPump.angle = 90;
-            downPump.enableBody = true;
-            game.physics.arcade.enable(downPump);
-            downPump.body.velocity.y = 1000;
-            game.physics.arcade.overlap(downPump, pookah1, killPookah1);
-            game.physics.arcade.overlap(downPump, pookah2, killPookah2);
-            game.physics.arcade.overlap(downPump, pookah3, killPookah3);
-            game.physics.arcade.overlap(downPump, fygar1, killFygar1);
-            game.physics.arcade.overlap(downPump, fygar2, killFygar2);
+            if (!pumpExists) {
+                	$("#pump").get(0).play(); //gets the first element of the sound
+                pump = game.add.sprite(digger.x, digger.y, 'pump');
+                pump.enableBody = true;
+                game.physics.arcade.enable(pump);
+                pump.physicsBodyType = Phaser.Physics.ARCADE;
+                pump.body.velocity.x = 1000;
+                pumpExists = true;
+                game.time.events.add(Phaser.Timer.SECOND * 1, reload);
+            }
         }
     }
 
     if (trackFacing == 2) {
         if (pumpButton.isDown) {
-            leftPump = pump.create(digger.x, digger.y, 'pump');
-            leftPump.angle = 180;
-            leftPump.enableBody = true;
-            game.physics.arcade.enable(leftPump);
-            leftPump.body.velocity.x = -1000;
-            if (digger.x > 50)
-                leftPump.x -= speed;
-            leftPump.body.velocity.x = -1000;
-            leftPump.angle = 180;
-            game.physics.arcade.overlap(leftPump, pookah1, killPookah1);
-            game.physics.arcade.overlap(leftPump, pookah2, killPookah2);
-            game.physics.arcade.overlap(leftPump, pookah3, killPookah3);
-            game.physics.arcade.overlap(leftPump, fygar1, killFygar1);
-            game.physics.arcade.overlap(leftPump, fygar2, killFygar2);
+            if (!pumpExists) {
+                	$("#pump").get(0).play(); //gets the first element of the sound
+                pump = game.add.sprite(digger.x, digger.y, 'pump');
+                pump.angle = 90;
+                pump.enableBody = true;
+                game.physics.arcade.enable(pump);
+                pump.physicsBodyType = Phaser.Physics.ARCADE;
+                pump.body.velocity.y = 1000;
+                pumpExists = true;
+                game.time.events.add(Phaser.Timer.SECOND * 1, reload);
+            }
         }
     }
 
     if (trackFacing == 3) {
         if (pumpButton.isDown) {
-            rightPump = pump.create(digger.x, digger.y, 'pump');
-            rightPump.enableBody = true;
-            game.physics.arcade.enable(rightPump);
-            rightPump.body.velocity.x = 1000;
-            game.physics.arcade.overlap(rightPump, pookah1, killPookah1);
-            game.physics.arcade.overlap(rightPump, pookah2, killPookah2);
-            game.physics.arcade.overlap(rightPump, pookah3, killPookah3);
-            game.physics.arcade.overlap(rightPump, fygar1, killFygar1);
-            game.physics.arcade.overlap(rightPump, fygar2, killFygar2);
+            if (!pumpExists) {
+                	$("#pump").get(0).play(); //gets the first element of the sound
+                pump = game.add.sprite(digger.x, digger.y, 'pump');
+                pump.angle = 180;
+                pump.enableBody = true;
+                game.physics.arcade.enable(pump);
+                pump.physicsBodyType = Phaser.Physics.ARCADE;
+                pump.body.velocity.x = -1000;
+                pumpExists = true;
+                game.time.events.add(Phaser.Timer.SECOND * 1, reload);
+            }
         }
     }
 
 
-};
+    game.physics.arcade.overlap(pump, pookah1, killPookah1);
+    game.physics.arcade.overlap(pump, pookah2, killPookah2);
+    game.physics.arcade.overlap(pump, pookah3, killPookah3);
+    game.physics.arcade.overlap(pump, fygar1, killFygar1);
+    game.physics.arcade.overlap(pump, fygar2, killFygar2);
 
-function killPookah1() {
-    pookah1.kill();
-    clear += 1;
-    console.log(clear);
-    if (clear.length == 5) {
-        win = winner.create(100, 100, 'winner');
-        // $("#winNoise").get(0).play();	//gets the first element of the sound
-        speed = 9;
+
+
+    function killPookah1() {
+        pookah1.kill();
+        clear += 1;
+        console.log(clear);
+        if (clear == 5) {
+            win = winner.create(100, 100, 'winner');
+            $("#win").get(0).play(); //gets the first element of the sound
+            speed = 9;
+        }
+    }
+
+    function killPookah2() {
+        pookah2.kill();
+        clear += 1;
+        console.log(clear);
+        if (clear == 5) {
+            win = winner.create(100, 100, 'winner');
+            $("#win").get(0).play(); //gets the first element of the sound
+            // $("#winNoise").get(0).play();
+            speed = 9;
+        }
+    }
+
+    function killPookah3() {
+        pookah3.kill();
+        clear += 1;
+        console.log(clear);
+        if (clear == 5) {
+            win = winner.create(100, 100, 'winner');
+            $("#win").get(0).play(); //gets the first element of the sound
+            // $("#winNoise").get(0).play();
+            speed = 9;
+        }
+    }
+
+    function killFygar1() {
+        fygar1.kill();
+        clear += 1;
+        console.log(clear);
+        if (clear == 5) {
+            win = winner.create(100, 100, 'winner');
+            $("#win").get(0).play(); //gets the first element of the sound
+            // $("#winNoise").get(0).play();
+            speed = 9;
+        }
+    }
+
+    function killFygar2() {
+        fygar2.kill();
+        clear += 1;
+        console.log(clear);
+        if (clear == 5) {
+            win = winner.create(100, 100, 'winner');
+            $("#win").get(0).play(); //gets the first element of the sound
+            // $("#winNoise").get(0).play();
+            speed = 9;
+        }
+    }
+
+    function digSoil() {
+        map.putTile(-1, layer.getTileX(digger.x), layer.getTileY(digger.y));
+    }
+
+    function reload() {
+        pumpExists = false;
+    }
+
+    function PlaySound(soundObj) {
+    sound = document.getElementById(soundObj);
+    sound.Play();
     }
 }
 
-function killPookah2() {
-    pookah2.kill();
-    clear += 1;
-    console.log(clear);
-    if (clear.length == 5) {
-        win = winner.create(100, 100, 'winner');
-        // $("#winNoise").get(0).play();
-        speed = 9;
-    }
-}
-
-function killPookah3() {
-    pookah3.kill();
-    clear += 1;
-    console.log(clear);
-    if (clear.length == 5) {
-        win = winner.create(100, 100, 'winner');
-        // $("#winNoise").get(0).play();
-        speed = 9;
-    }
-}
-
-function killFygar1() {
-    fygar1.kill();
-    clear += 1;
-    console.log(clear);
-    if (clear.length == 5) {
-        win = winner.create(100, 100, 'winner');
-        // $("#winNoise").get(0).play();
-        speed = 9;
-    }
-}
-
-function killFygar2() {
-    fygar2.kill();
-    clear += 1;
-    console.log(clear);
-    if (clear.length == 5) {
-        win = winner.create(100, 100, 'winner');
-        // $("#winNoise").get(0).play();
-        speed = 9;
-    }
-}
-
-function digSoil() {
-    map.putTile(-1, layer.getTileX(digger.x), layer.getTileY(digger.y));
-}
-
-
-// SOUND BITE
-// function PlaySound(noise) {
-//   var sound = document.getElementById(noise);
-//   sound.Play();
-// }
+//THIS FUNCTION WILL PASS THE SOUND TO YOUR EVENT WHEN IT IS CLICKED
+//(MUST HAVE JQUERY LIBRARY SCRIPT LINKED)
